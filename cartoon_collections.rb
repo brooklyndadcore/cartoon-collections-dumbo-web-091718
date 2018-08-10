b84e0f4cfebbcde72ffe77bd67c97997babbbdd2
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-10 19:04:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)
+   arr.each_with_index {|name, idx| puts"#{idx+1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(arr)
+  return arr.collect do |word| 
+    word[0].upcase + word[1..-1] + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.each do |word|
+    if word.length > 4 
+      return true 
+    else 
+      return false
+    end
 end
 
 def find_the_cheese# code an argument here
