e6fedaded8cd0971ec3a5833c2eec842bc13a047
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-10 19:09:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |word|
     if cheese_types.include?(word)
-      return word
+      return array[word]
     else
       return nil
     end
